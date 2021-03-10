6cf12eb03c94f685d53d7f4ca19c6bc66c3c9702
AmitHaim1 <79083589+AmitHaim1@users.noreply.github.com>
2021-03-10 11:39:49 +0200

    Add files via upload

---
+++ stix_shifter_modules/cbcloud/stix_translation/transformers.py
@@ -0,0 +1,62 @@
+from stix_shifter_utils.stix_translation.src.utils.transformers import ValueTransformer
+from stix_shifter_utils.utils import logger
+import base64
+
+LOGGER = logger.set_logger(__name__)
+
+
+class ExtractSHA256ValueFromArray(ValueTransformer):
+    """A value transformer to convert QRadar ObjectName to windows-registry-key.key STIX"""
+
+    @staticmethod
+    def transform(registry):
+
+        try:
+            val1 = registry[0]
+            val2 = None
+            if len(registry) > 1:
+                val2 = registry[1]
+            return val1 if len(val1) == 64 else val2
+        except ValueError:
+            LOGGER.error("Cannot convert root key to Stix formatted windows registry key")
+
+
+class ExtractMD5ValueFromArray(ValueTransformer):
+    @staticmethod
+    def transform(registry):
+
+        try:
+            val1 = registry[0]
+            val2 = None
+            if len(registry) > 1:
+                val2 = registry[1]
+            return val1 if len(val1) == 32 else val2
+        except ValueError:
+            LOGGER.error("Cannot convert root key to Stix formatted windows registry key")
+
+
+class ToBase64(ValueTransformer):
+    """A value transformer for expected base 64 values"""
+
+    @staticmethod
+    def transform(obj):
+        try:
+            return base64.b64encode(obj.encode()).decode('ascii')
+        except ValueError:
+            LOGGER.error("Cannot convert input to base64")
+
+
+class ToIPAddress(ValueTransformer):
+    """A value transformer for expected base 64 values"""
+
+    @staticmethod
+    def transform(obj):
+        try:
+            if type(obj) is int:
+                return 'XXX'
+            return 'YYY'
+        except ValueError:
+            LOGGER.error("Cannot convert input to base64")
+
+
+
